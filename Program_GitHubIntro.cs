96c60a2cc319c16ba6a64524a96e6478d0983ef2
manuharihara <manuharihara@LAPTOP-MOSC69FM>
2020-06-24 22:44:49 -0400

    Add project files.

---
+++ Program_GitHubIntro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GitHubIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This Is Manoj Github inclass Excercise");
+
+
+        }
+    }
+}

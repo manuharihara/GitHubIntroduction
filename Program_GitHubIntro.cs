c1b7d0904b6976b5ac67d22f06ecbe56d45314cf
Jerry Marks <ibjerrymarks7@gmail.com>
2020-06-26 15:05:03 -0500

    This is my addition to Manoj's code

--- Program_GitHubIntro.cs
+++ Program_GitHubIntro.cs
@@ -8,6 +8,7 @@ namespace GitHubIntro
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("This Is Manoj Github inclass Excercise");
+            Console.WriteLine("This is Jerry saying hi to Manoj");
 
 
         }
